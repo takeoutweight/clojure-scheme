c2678215a4f432d87482060e39f6456d37ea78e7
fogus <mefogus@gmail.com>
2011-09-02 14:09:04 -0400

    Initial version of extend-object. Works.

--- src/cljs/cljs/core.cljs
+++ src/cljs/cljs/core.cljs
@@ -3269,4 +3269,7 @@ reduces them without incurring seq initialization"
 (defn extend-object
   ""
   [obj fn-map]
-  )
+  (doseq [[key-name f] fn-map]
+    (let [str-name (name key-name)
+          bind-fn (goog.bind f obj)]
+      (js* "~{obj}[~{str-name}] = ~{bind-fn}"))))
