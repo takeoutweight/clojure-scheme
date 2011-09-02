359d1fd256273030ad3f90b95b7d194fa351cc9c
fogus <mefogus@gmail.com>
2011-09-02 14:03:15 -0400

    Initial version of extend-object. Works.

--- src/cljs/cljs/core.cljs
+++ src/cljs/cljs/core.cljs
@@ -3213,3 +3213,8 @@ reduces them without incurring seq initialization"
 (defn prefers
   "Given a multimethod, returns a map of preferred value -> set of other values"
   [multifn] (-prefers multifn))
+
+(defn extend-object
+  ""
+  [obj fn-map]
+  )
