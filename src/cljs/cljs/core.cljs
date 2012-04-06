b7211630a504ad4792912998480332dda1908602
takeoutweight <nathan.d.sorenson@gmail.com>
2012-04-06 00:55:34 -0700

    compare-and-set use field access like everyone else.

--- src/cljs/cljs/core.cljs
+++ src/cljs/cljs/core.cljs
@@ -3365,7 +3365,7 @@ reduces them without incurring seq initialization"
   current value of the atom is identical to oldval. Returns true if
   set happened, else false."
   [a oldval newval]
-  (if (= a.state oldval)
+  (if (= (.-state a) oldval)
     (do (reset! a newval) true)
     false))
 
