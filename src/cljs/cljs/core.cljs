d249a6d41bd562f0ea1d3dafae561409401a9bd3
takeoutweight <nathan.d.sorenson@gmail.com>
2013-02-26 19:57:30 -0800

    fix infinite range upper bound literal.

--- src/cljs/cljs/core.cljs
+++ src/cljs/cljs/core.cljs
@@ -6384,7 +6384,7 @@ reduces them without incurring seq initialization"
   "Returns a lazy seq of nums from start (inclusive) to end
    (exclusive), by step, where start defaults to 0, step to 1,
    and end to infinity."
-  ([] (range 0 (scm* [] "+inf.0") 1)) ;js/Number.MAX_VALUE 1
+  ([] (range 0 (scm-str* "+inf.0") 1)) ;js/Number.MAX_VALUE 1
   ([end] (range 0 end 1))
   ([start end] (range start end 1))
   ([start end step] (Range. nil start end step nil)))
